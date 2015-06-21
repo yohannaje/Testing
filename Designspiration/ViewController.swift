a6d0754af2353e8040e505fcd350ba6372463b41
Bunny <Bunny@Cosmos.local>
2015-06-20 22:29:15 -0300

    Initial Commit

---
+++ Designspiration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Designspiration
+//
+//  Created by Yoh on 6/20/15.
+//  Copyright (c) 2015 Harmony Bunny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
